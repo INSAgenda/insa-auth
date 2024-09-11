dec285e530d2e42ea67c76d15760a0adb213ba01
Mubelotix <mubelotix@gmail.com>
2024-09-11 08:30:24 +0200

    Suppress warning

--- src/provider/mod.rs
+++ src/provider/mod.rs
@@ -7,6 +7,7 @@ use crate::*;
 use rocket::tokio::sync::RwLock;
 use std::{collections::HashMap, sync::LazyLock};
 
+#[allow(clippy::type_complexity)]
 static TICKETS: LazyLock<RwLock<HashMap<String, (u64, String, Claims)>>> = LazyLock::new(|| {
     RwLock::new(HashMap::new())
 });
